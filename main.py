f8bb2dc16d7b1bfeb2372e635f5bda9e0d151037
Andrew Gordienko <60705784+AndrewGordienko@users.noreply.github.com>
2022-06-20 15:30:09 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,12 +4,15 @@ import random
 import numpy as np
 import math
 
-env = gym.make('CartPole-v1')
-observation_space = env.observation_space.shape[0]
-action_space = env.action_space.n
-all_connections = []
+# project is to use the NEAT algorithm to learn 
 
-class Node_Genes():
+# gym is a library to generate the enviornment that the algorithm will run in
+env = gym.make('CartPole-v1') # cartpole is the enviornment that this will be tested
+observation_space = env.observation_space.shape[0] # integer number for how many inputs are given
+action_space = env.action_space.n # how many possible outputs there are in the enviornment 
+all_connections = [] # where all connections are stored for innovation numnber
+
+class Node_Genes(): # this is the neuron of the neural network
     def __init__(self):
         self.number = None
         self.layer_number = None
@@ -17,7 +20,7 @@ class Node_Genes():
         self.value = 0
         self.connections_out = []
 
-class Connect_Genes():
+class Connect_Genes(): # this is what the connection between neurons will look like/where the info will be stored
     def __init__(self):
         self.output_node_number = None
         self.output_node_layer = None
@@ -25,28 +28,29 @@ class Connect_Genes():
         self.enabled = True
         self.innovation = None
 
-class Agent():
+class Agent(): 
     def __init__(self):
-        self.network = [[], []]
-        self.connections = []
+        self.network = [[], []] # network is a 2d array
+        self.connections = [] 
         self.connections_weight = []
         self.node_count = 0
         self.all_nodes = []
         self.fitness = 0
 
-        for i in range(observation_space):
+        for i in range(observation_space): # making a node for every input the network will take in and adding it to the first level of the array
             node = Node_Genes()
             node.number = i
             node.layer_number = 0
             self.network[0].append(deepcopy(node))
             self.node_count += 1
-        for i in range(action_space):
+        for i in range(action_space): # making a node for every output the environment has
             node = Node_Genes()
             node.number = observation_space + i
             node.layer_number = 1
             self.network[1].append(deepcopy(node))
             self.node_count += 1
 
+        # the next part of code is connecting every input node to every output node while also initializing important things about it
         for i in range(len(self.network[0])):
             for j in range(len(self.network[1])):
                 connection = Connect_Genes()
@@ -56,12 +60,12 @@ class Agent():
                 self.connections.append([self.network[0][i].number, observation_space + j])
                 self.connections_weight.append(connection.weight)
                 self.network[0][i].connections_out.append(deepcopy(connection))
-
-    def ReLU(self, x):
+ 
+    def ReLU(self, x): # this is the relu function which is important in all neural networks
         return x * (x > 0)
 
-    def feed_forward(self, state):
-        # sweep the entire network setting all values to 0
+    def feed_forward(self, state): # when given a network we want to figure out the outputs to it
+        # sweep the entire network setting all values to 0 to make sure nothing is left over
         for i in range(len(self.network)):
             for j in range(len(self.network[i])):
                 self.network[i][j].value = 0
@@ -86,21 +90,23 @@ class Agent():
                         # find target node    
                         for p in range(len(self.network[output_node_layer])):
                             if self.network[output_node_layer][p].number == output_node_number:
-                                self.network[output_node_layer][p].value += self.network[i][j].value * weight
+                                self.network[output_node_layer][p].value += self.network[i][j].value * weight # perform the calculation based on the value from the last node connected to the current one
 
-        # each layer thats not first
+        # each layer thats not first since this is the input layer
         for i in range(1, len(self.network)):
             for j in range(len(self.network[i])):
-                self.network[i][j].value = self.ReLU(self.network[i][j].value) + self.network[i][j].bias
+                self.network[i][j].value = self.ReLU(self.network[i][j].value) + self.network[i][j].bias # normalizing all the values
         
         last_values = []
         maximum_index = len(self.network)-1
         for i in range(len(self.network[maximum_index])):
-            last_values.append(self.network[maximum_index][i].value)
+            last_values.append(self.network[maximum_index][i].value) # returning the last two values of the network
         
         return last_values
 
-    def finding_innovation(self, starting_node_number, ending_node_number):
+    def finding_innovation(self, starting_node_number, ending_node_number): # each connection has an innovation number and this is this part
+        # innovation numbers are unique to the conceptual pairing of two nodes and they are unique, innovation number scannot appear twice
+        # so we are looking for the connection with that number
         connection_innovation = [starting_node_number, ending_node_number]
 
         if connection_innovation not in all_connections:
@@ -110,6 +116,7 @@ class Agent():
         return innovation_number
 
     def mutate_node(self): # adding node between two connected
+        # first block of code picks a random node thats not on the very last layer since you cant connect the last layer to anything else
         random_layer = random.randint(0, len(self.network)-2)
         random_node_index = random.randint(0, len(self.network[random_layer])-1)
         random_node = self.network[random_layer][random_node_index]
@@ -121,8 +128,9 @@ class Agent():
         output_node_number = random_connection.output_node_number
         output_node_layer = random_connection.output_node_layer
         
-        if abs(random_layer - output_node_layer) == 1:
+        if abs(random_layer - output_node_layer) == 1: # checking that there is a layer that exists already between the two, a new layer has to be added here
             # print("added layer")
+            # code here makes a new node and then two connections, one into the node, and one out of the node
             self.network.insert(random_layer + 1, [])
             node = Node_Genes()
             node.number = self.node_count
@@ -157,7 +165,8 @@ class Agent():
                             self.network[i][j].connections_out[k].output_node_layer += 1
             
         else:
-            # print("minimum layer")
+            # a new layer does not to be added
+            # same method - node made, two connections
             node = Node_Genes()
             node.number = self.node_count
             self.node_count += 1
@@ -186,6 +195,7 @@ class Agent():
             connected = True
 
             while connected == True:
+                # find two nodes randomly that are not on the same layer and connect them
                 first_random_layer = random.randint(0, len(self.network)-2)
                 first_random_node_index = random.randint(0, len(self.network[first_random_layer])-1)
                 first_random_node = self.network[first_random_layer][first_random_node_index]
@@ -216,7 +226,7 @@ class Agent():
                     self.network[first_random_layer][first_random_node_index].connections_out.append(deepcopy(connection))
                              
 
-    def mutate_enable_disable(self):
+    def mutate_enable_disable(self): # randomly pick a node whos connection to enable or disable 
         random_layer = random.randint(0, len(self.network)-2)
         random_node_index = random.randint(0, len(self.network[random_layer])-1)
         random_node = self.network[random_layer][random_node_index]
@@ -229,10 +239,7 @@ class Agent():
 
     def mutate_weight_shift(self):
         #print("mutate weight shift")
-        #self.printing_stats()
-        #print("--")
-        #print(self.connections)
-        #print("--")
+        # pick a random node and multiply the weight by a random float between 0 and 2
         random_layer = random.randint(0, len(self.network)-2)
         random_node_index = random.randint(0, len(self.network[random_layer])-1)
         random_node = self.network[random_layer][random_node_index]
@@ -246,6 +253,7 @@ class Agent():
         self.connections_weight[number_index] *= number
 
     def mutate_weight_random(self):
+        # same thing as above except no multiplication, its just being overriden
         random_layer = random.randint(0, len(self.network)-2)
         random_node_index = random.randint(0, len(self.network[random_layer])-1)
         random_node = self.network[random_layer][random_node_index]
@@ -260,7 +268,7 @@ class Agent():
 
     def mutation(self):
         choice = random.randint(0, 10)
-
+        # this is where the mutation happens but also i can make some stuff be more likely to be selected than others
         if choice == 0:
             #print("mutate link")
             self.mutate_link()
@@ -278,11 +286,13 @@ class Agent():
             self.mutate_weight_random()
 
     def choose_action(self, state):
+        # its in the name, just choose the last value with the biggest value as the action
         last_values = self.feed_forward(state)
         action = np.argmax(last_values)
         return action
     
     def printing_stats(self):
+        # this was just for me to print out the entire network and all the connections to see that everything works
         for i in range(len(self.network)):
             print("--")
             print("layer {}".format(i))
@@ -294,16 +304,17 @@ class Agent():
                         print("layer {} number {} innovation {}".format(self.network[i][j].connections_out[k].output_node_layer, self.network[i][j].connections_out[k].output_node_number, self.network[i][j].connections_out[k].innovation))
 
     def speciation(self, species1, species2):
-        N = max(len(species1.connections), len(species2.connections))
-        E = abs(species1.node_count - species2.node_count)
+        # so theres a calculation for how similar two agents are to see if they should be in the same species
+        N = max(len(species1.connections), len(species2.connections)) # who has the most connections
+        E = abs(species1.node_count - species2.node_count) # difference in node count
 
-        D = 0
+        D = 0 # how many connections does each not network share
         both = species1.connections + species2.connections
         for i in range(len(both)):
             if both.count(both[i]) == 1:
                 D += 1
         
-        W = 0
+        W = 0 # sum of weight differences for connections shared
         shorter_species = species1.connections
         if species1.connections <= species2.connections:
             shorter_species = species1.connections
@@ -325,23 +336,14 @@ class Agent():
         #print(species1.fitness)
         #print(species2.fitness)
 
-        """
-        self.network = [[], []]
-        self.connections = []
-        self.connections_weight = []
-        self.node_count = 0
-        self.all_nodes = []
-        self.fitness = 0
-        """
-
         parents = [species1, species2]
         index = np.argmax([int(species1.fitness), int(species2.fitness)])
-        fit_parent = parents[index]
+        fit_parent = parents[index] # take the fitest parent
         less_fit_parent = parents[abs(1-index)]
 
-        child = Agent()
-        child.network = fit_parent.network
-        child.node_count = fit_parent.node_count
+        child = Agent() 
+        child.network = fit_parent.network # take all the nodes from fittest parent
+        child.node_count = fit_parent.node_count 
         
         both = species1.connections + species2.connections
         connections_both = []
@@ -356,12 +358,13 @@ class Agent():
         print(species2.connections_weight)
         """
 
-        for i in range(len(both)):
+        for i in range(len(both)): 
             if both.count(both[i]) == 2:
                 if both[i] not in connections_both:
                     connections_both.append(both[i])
 
-        for i in range(len(connections_both)):
+        for i in range(len(connections_both)): # for connections shared by both its 50/50 of whos you take basically
+            # the rest of the code addresses this finding out if its enabled and then taking it 50/50 for connections shared
             parent_chosen.append(random.randint(0, 1))
 
         #print("connections in both")
@@ -419,16 +422,16 @@ epochs = 10
 
 for i in range(network_amount):
     agent = Agent()
-    for i in range(5):
+    for i in range(5): # start by mutating 5 times
         agent.mutation()
 
     networks.append(deepcopy(agent))
 
-for e in range(epochs):
+for e in range(epochs): # some amount of generations
     species = []
     average_general_fitness = 0
 
-    for i in range(len(networks)):
+    for i in range(len(networks)): # play through each network
         agent = networks[i]
         state = env.reset()
         state = np.reshape(state, [1, observation_space])
@@ -445,7 +448,7 @@ for e in range(epochs):
             if done:
                 #agent.printing_stats()
                 print("score {}".format(score))
-                agent.fitness = score
+                agent.fitness = score # this is important later for making kids
                 average_general_fitness += score
 
                 if score > best_score:
@@ -455,7 +458,7 @@ for e in range(epochs):
                 break
     
     average_general_fitness /= network_amount
-    networks.append(best_agent)
+    networks.append(best_agent) # always keep the best agent
     print("epoch {} best score {} average fitness {}".format(e, best_score, average_general_fitness))
     
     species.append([networks[0]])
@@ -463,14 +466,15 @@ for e in range(epochs):
     for i in range(1, len(networks)):
         added = False
         for j in range(len(species)):
-            if agent.speciation(species[j][0], networks[i]) >= threshold_species:
+            if agent.speciation(species[j][0], networks[i]) >= threshold_species: # create the species based on if its higher than the treshold
                 species[j].append(networks[i])
                 added = True
                 break
 
         if added == False:
             species.append([networks[i]])
-
+    
+    # the code here is all about cutting the worse 50%
     for i in range(len(species)):
         species[i].sort(key=lambda x: x.fitness, reverse=True)
 
@@ -491,22 +495,22 @@ for e in range(epochs):
         isolated_average /= len(species[i])
         species_average_fitness.append(isolated_average)
 
-        amount = math.ceil(isolated_average/average_general_fitness * len(species[i]))
+        amount = math.ceil(isolated_average/average_general_fitness * len(species[i])) # there is a calculation how many kids each species should have based on how it performs relatively 
 
         for j in range(amount):
-            if amount == 1 or len(species[i]) == 1:
+            if amount == 1 or len(species[i]) == 1: # if only one network in species keep it
                 new_networks.append(species[i][0])
                 break
             else:
-                generated = random.sample(species[i], 2)
+                generated = random.sample(species[i], 2) # else randomly make a new kid based off two parents that exist in the species
                 first_parent = generated[0]
                 second_parent = generated[1]
 
-                child = agent.making_children(first_parent, second_parent)
+                child = agent.making_children(first_parent, second_parent) # make child and add it
                 new_networks.append(deepcopy(child))
 
     for i in range(len(new_networks)):
-        new_networks[i].mutation()
+        new_networks[i].mutation() # mutate everyone
 
     new_networks.append(deepcopy(best_agent))
-    networks = new_networks
+    networks = new_networks # put the new networks in the networks list and run it again
